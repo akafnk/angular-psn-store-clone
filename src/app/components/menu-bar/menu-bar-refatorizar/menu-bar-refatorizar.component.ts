2b3225d43634abd827d25ad9fed5c1d9b5091254
Frank Gárate <frank.garaterios@gmail.com>
2023-10-05 12:41:39 -0300

    Refatoração igual foi feito em aula

---
+++ src/app/components/menu-bar/menu-bar-refatorizar/menu-bar-refatorizar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-bar-refatorizar',
+  templateUrl: './menu-bar-refatorizar.component.html',
+  styleUrls: ['./menu-bar-refatorizar.component.css']
+})
+export class MenuBarRefatorizarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
